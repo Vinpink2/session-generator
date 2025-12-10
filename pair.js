dcd66ab69e8d39303888cea0863b81c11d9c4144
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-12-10 10:03:24 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
                 version: [2, 3000, 1027934701],
                 printQRInTerminal: false,
                 logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
-                browser: Browsers.windows('Edge'),
+                browser: ["Windows", "Firefox", "130.0.1"],
             });
 
             if (!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
