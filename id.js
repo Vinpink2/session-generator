fa46aaf63e7434808ad5c25a93cd92d80781bf71
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2026-03-17 09:41:24 +0300

    Delete id.js

--- id.js
+++
@@ -1,9 +0,0 @@
-const { randomUUID } = require('crypto');
-
-const SESSION_PREFIX = 'JUNE-MD:~';
-
-function makeid() {
-  return randomUUID().replace(/-/g, '').slice(0, 16);
-}
-
-module.exports = { makeid, SESSION_PREFIX };
